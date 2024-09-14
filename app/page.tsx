9a85766e8ac43ffaf7465d81ff220e4fed702c94
jorgennonstad <jorgenon@stud.ntnu.no>
2024-09-14 17:15:08 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -535,7 +535,7 @@ export default function Home() {
       <footer>
         <h2><img className='footerLogo' src="/faviconWhite.png" alt="Logo" /> Deviro</h2>
         <ul>
-          <li><span>Adr: </span><a href="https://www.google.com/maps/place/Storgata+2,+1821,+2815+Gj%C3%B8vik/@60.7961651,10.6904173,17z/data=!3m1!4b1!4m5!3m4!1s0x4641da3d35be1823:0x7108834441c40703!8m2!3d60.7961625!4d10.6929922?entry=ttu&g_ep=EgoyMDI0MDkxMC4wIKXMDSoASAFQAw%3D%3D" target='_blank'>Storgata 2, 1821 Gjøvik</a></li>
+          <li><span>Adr: </span><a href="https://www.google.com/maps/place/Storgata+2,+1821,+2815+Gj%C3%B8vik/@60.7961651,10.6904173,17z/data=!3m1!4b1!4m5!3m4!1s0x4641da3d35be1823:0x7108834441c40703!8m2!3d60.7961625!4d10.6929922?entry=ttu&g_ep=EgoyMDI0MDkxMC4wIKXMDSoASAFQAw%3D%3D" target='_blank'>Storgata 2, 2815 Gjøvik</a></li>
           <li><span>Tlf: </span><a href='tel:+4793434688'>+47 934 34 688</a> {/* tel for phone */}</li>
           <li><span>Orgnr:</span> 4206969</li>
         </ul>
